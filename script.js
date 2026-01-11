89a74325dd99944f0236cdf6230ac58333f6212b
RolyMorvern <141121305+RolyMorvern@users.noreply.github.com>
2026-01-11 01:01:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -7,10 +7,29 @@ $("#menuPhone").click(() => {
     	$("body").css("overflow", "visible")
     }
 });
+
 function setNavHeight() {
     const navHeight = $('nav').outerHeight();
     $('html').css('--nav-height', navHeight + 'px');
 }
 
+function updatePrice (price) {
+    $("#itemPrice").text(`€${price}`)
+}
+
 $(window).on('load resize', setNavHeight);
 
+function changeSize() {
+    $('.size').removeClass('activeSize');
+    $(this).addClass('activeSize');
+}
+
+$(document).on('click', '.size', function() {
+    $('.size').removeClass('activeSize');
+    $(this).addClass('activeSize');
+});
+
+$(document).on('click', '.color', function() {
+    $('.colorContainer > div.active').contents().unwrap();
+    $(this).wrap('<div class="active"></div>');
+});
